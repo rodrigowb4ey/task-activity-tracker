6e3f82ae21194dfaf0e25d00380c90589acc5fee
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-03-27 09:06:26 -0300

    Addition of the first Vue components (SideBar, TaskForm)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-fifth">
+      <SideBar />
+    </div>
+    <div class="column is-four-fifth">
+      <TaskForm />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import SideBar from "./components/SideBar.vue"
+import TaskForm from "./components/TaskForm.vue"
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    SideBar,
+    TaskForm
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Form to create a new task"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Which task do you wish to start?"
+        />
+      </div>
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong> 00:00:00 </strong>
+          </section>
+          <button class="button">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>Play</span>
+          </button>
+          <button class="button">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>Stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TaskForm",
+});
+</script>
