e60a6f0fc15e6fa8fa479a961bcb6fa788779d5f
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-04-04 16:57:38 -0300

    Rendering list

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,46 @@
     <div class="column is-four-fifth">
       <TaskForm @whenSavingTask="saveTask" />
       <div class="lista">
-        <NewTask v-for="(task, index) in taskList" :key="index" :task="task"/>
+        <NewTask v-for="(task, index) in taskList" :key="index" :task="task" />
+        <TaskBox v-if="isListEmpty">
+          You're not feeling very productive today, are you? :[
+        </TaskBox>
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import SideBar from "./components/SideBar.vue"
-import TaskForm from "./components/TaskForm.vue"
-import NewTask from "./components/NewTask.vue"
-import TaskInterface from "./interfaces/TaskInterface"
+import { defineComponent } from "vue";
+import SideBar from "./components/SideBar.vue";
+import TaskForm from "./components/TaskForm.vue";
+import TaskBox from "./components/TaskBox.vue";
+import NewTask from "./components/NewTask.vue";
+import TaskInterface from "./interfaces/TaskInterface";
 
 export default defineComponent({
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
-      taskList: [] as TaskInterface[]
+      taskList: [] as TaskInterface[],
+    };
+  },
+  computed: {
+    isListEmpty () : boolean {
+      return this.taskList.length === 0;
     }
   },
   components: {
     SideBar,
     TaskForm,
+    TaskBox,
     NewTask,
   },
   methods: {
     saveTask(task: TaskInterface) {
       this.taskList.push(task);
-    }
-  }
+    },
+  },
 });
 </script>
 
@@ -43,5 +53,4 @@ export default defineComponent({
 .lista {
   padding: 1.25rem;
 }
-
 </style>
--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <TaskBox>
     <div class="columns">
-      <div class="column is-7">{{ task.description }}</div>
+      <div class="column is-7">
+        {{ task.description || "Task without description" }}
+      </div>
       <div class="column">
         <TaskCronometer :timeInSeconds="task.durationInSeconds" />
       </div>
     </div>
-  </div>
+  </TaskBox>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import TaskCronometer from "./TaskCronometer.vue";
 import TaskInterface from "../interfaces/TaskInterface";
+import TaskBox from "./TaskBox.vue";
 
 export default defineComponent({
   name: "NewTask",
   components: {
     TaskCronometer,
+    TaskBox,
   },
   props: {
     task: {
       type: Object as PropType<TaskInterface>,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 });
 </script>
 
 <style scoped>
-.box {
-  background: #FAF0CA;
-}
 </style>
