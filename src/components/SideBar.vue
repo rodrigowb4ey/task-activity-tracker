c9e766354ea85182048ebe16a1e7041fac60f6a2
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-04-06 20:30:47 -0300

    Added nav bar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,56 +1,85 @@
 <template>
-    <header>
-        <h1>
-            <img src="../assets/aluratrackerlogo.png">
-        </h1>
-        <button class="button" @click="changeTheme">
-            {{ buttonText }}
-        </button>
-    </header>
+  <header>
+    <h1>
+      <img src="../assets/aluratrackerlogo.png" />
+    </h1>
+    <div class="has-text-centered">
+      <button class="button" @click="changeTheme">
+        {{ buttonText }}
+      </button>
+      <nav class="panel mt-5">
+          <ul>
+              <li>
+                  <router-link to="/" class="link">
+                      <i class="fas fa-tasks"></i>
+                      Tasks
+                  </router-link>
+              </li>
+              <li>
+                  <router-link to="/projects" class="link">
+                      <i class="fas fa-project-diagram"></i>
+                      Projects
+                  </router-link>
+              </li>
+          </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'SideBar',
-    emits: ['whenChangingTheme'],
-    data() {
-        return {
-            darkModeIsActive: false
-        }
+  name: "SideBar",
+  emits: ["whenChangingTheme"],
+  data() {
+    return {
+      darkModeIsActive: false,
+    };
+  },
+  computed: {
+    buttonText() {
+      if (this.darkModeIsActive) {
+        return "Light Theme";
+      } else {
+        return "Dark Theme";
+      }
     },
-    computed: {
-        buttonText() {
-            if (this.darkModeIsActive) {
-                return 'Light Theme'
-            } else {
-                return 'Dark Theme'
-            }
-        }
+  },
+  methods: {
+    changeTheme() {
+      this.darkModeIsActive = !this.darkModeIsActive;
+      this.$emit("whenChangingTheme", this.darkModeIsActive);
     },
-    methods: {
-        changeTheme() {
-            this.darkModeIsActive = !this.darkModeIsActive;
-            this.$emit('whenChangingTheme', this.darkModeIsActive);
-        }
-    }
-})
-
+  },
+});
 </script>
 
 <style scoped>
 header {
-    padding: 1rem;
-    background: #0d3b66;
-    width: 100%;
-    height: 100vh;
-    text-align: center;
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
 }
 @media only screen and (max-width: 768px) {
-    header {
-        padding: 2.5rem;
-        height: auto;
-    }
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+.panel li {
+    margin: 8px 0;
+}
+.link {
+    color: #fff;
+}
+.link:hover {
+    color: #FAF0CA;
+}
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import TasksView from "../views/TasksView.vue"
+import ProjectsView from "../views/ProjectsView.vue"
 
-const routes: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tasks',
-    component: TasksView
-}];
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Tasks',
+        component: TasksView
+    },
+    {
+        path: '/projects',
+        name: 'Projects',
+        component: ProjectsView
+    }
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
